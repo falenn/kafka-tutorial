4bc8705cfda486705c56acebb1ac3eb3db8d1e25
Bates Curtis clbates <clbates@uwe.nsa.gov>
2022-03-14 01:35:35 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# kafka Usage Example
+
+## Docker / Docker Compose
+
+### Start Kafka
+```
+docker-compose up
+```
+
+### Interactive 
+Shell into kafka container
+```
+docker exec -it kafka_kafka_1
+```
+List topics
+```
+kafka-topics --bootstrap-server localhost:29092 --list
+```
+
+Create a topic
+```
+kafka-topics --bootstrap-server localhost:29092 --create --replication-factor 1 --partitions 1 --topic sample
+```
+
+Interact to see details - describe info on our test topic
+```
+kafka-topics --bootstrap-server localhost:29092 --describe --topic mytopic
+```
+
+# Test Data
+Generate 1KB file with null character, then base64 for use in sending to kafka
+```
+fallocate -l 1KB | base64 > 1kbfile.txt
+```
+
---
+++ producer.py
@@ -0,0 +1,35 @@
+from confluent_kafka import Producer
+from datetime import datetime
+from dateutil.tz import tzutc
+
+# Load data to send from file
+# read line-by-line, one message per line
+in_file = open('data.txt','r')
+data_source = in_file.readlines()
+
+p = Producer({'bootstrap.servers': 'localhost:29092'})
+
+def delivery_report(err, msg):
+    """ Called once for each message produced to indicate delivery result.
+        Triggered by poll() or flush(). """
+    if err is not None:
+        print('Message delivery failed: {}'.format(err))
+    else:
+        print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+count = 0
+for data in data_source:
+    # Trigger any available delivery report callbacks from previous produce() calls
+    p.poll(0)
+
+    count += 1
+    # Add id info to the meesages
+    data = "[" + datetime.now(tzutc()).isoformat() + "] " + str(count) + ": " + data
+    # Asynchronously produce a message, the delivery report callback
+    # will be triggered from poll() above, or flush() below, when the message has
+    # been successfully delivered or failed permanently.
+    p.produce('mytopic', data.encode('utf-8'), callback=delivery_report)
+
+# Wait for any outstanding messages to be delivered and delivery report
+# callbacks to be triggered.
+p.flush()
